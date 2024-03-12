4e5510bbd88d068879577792b77e4eefa386dbf7
Devinlin9 <devinlin@students.cs.ubc.ca>
2024-03-12 01:10:38 -0700

    Milestone 3 added

---
+++ src/ui/LoginUI.java
@@ -0,0 +1,53 @@
+package ui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class LoginUI extends JFrame {
+    private JFrame frame;
+    private JPanel panel;
+    private JLabel label_username, label_password;
+    private JTextField textField_username, passwordField_password;
+    private JButton button_login;
+    public LoginUI() {
+        frame = new JFrame("Login Page");
+        frame.setSize(540, 360);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        panel = new JPanel();
+
+        label_username = new JLabel("Username");
+        label_username.setFont(new Font("Arial", Font.PLAIN, 14));
+        label_username.setBounds(130, 110, 70, 20);
+        panel.add(label_username);
+
+        label_password = new JLabel("Password");
+        label_password.setFont(label_username.getFont());
+        label_password.setBounds(label_username.getX(), label_username.getY() + 40,
+                label_username.getWidth(), label_username.getHeight());
+        panel.add(label_password);
+
+        textField_username = new JTextField();
+        textField_username.setBounds(label_username.getX() + label_username.getWidth() + 20,
+                label_username.getY(), 120, label_username.getHeight());
+        panel.add(textField_username);
+
+        passwordField_password = new JPasswordField();
+        passwordField_password.setBounds(textField_username.getX(), label_password.getY(),
+                120, label_password.getHeight());
+        panel.add(passwordField_password);
+
+        button_login = new JButton("Login");
+        button_login.setBounds(textField_username.getX() + 20, label_username.getY() + 80, 80, 22);
+        button_login.setFocusPainted(false);
+        panel.add(button_login);
+
+        frame.add(panel);
+        panel.setLayout(null);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new LoginUI();
+    }
+}
