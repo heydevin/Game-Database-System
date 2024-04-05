e8e1b636eea7ac18c12d95a676932b20d558a460
Devinlin9 <devinlin@students.cs.ubc.ca>
2024-04-05 11:13:16 -0700

    Small bugs fixing

--- src/connecter/LoginConnector.java
+++ src/connecter/LoginConnector.java
@@ -9,12 +9,10 @@ import javax.swing.table.DefaultTableModel;
 public interface LoginConnector {
     void login(String username, String password);
     void switchPage(int pageIndex);
-
     User getUserFromSQL(String name);
     Account getAccountFromSQL(String email);
     void insertAccountIntoSQL(Account account);
     void insertCharacterIntoSQL(CharacterInfo character);
-
     DefaultTableModel groupByQuery();
     void deleteCharacterInfoFromSQL(String cName);
     void updateCharacterLevel(int newLevel,String cName);
